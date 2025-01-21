3946cac927aecd4b19b208eff1a643ddbe641e5f
aniket <50738821+enigma-24@users.noreply.github.com>
2025-01-20 22:28:29 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export default function App() {
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	return (
+		<>
+			<h1>give feedback</h1>
+			<button onClick={() => setGood((good) => good + 1)}>good</button>
+			<button onClick={() => setNeutral(neutral => neutral + 1)}>neutral</button>
+			<button onClick={() => setBad(bad => bad + 1)}>bad</button>
+			<h1>statistics</h1>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+		</>
+	);
+}
